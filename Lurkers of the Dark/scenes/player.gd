1ce29e795ba8c1c2a5c476b90ce0066742cccf53
BaconXBX <hugoca.2000@gmail.com>
2024-05-13 13:31:49 -0400

    zombie, take damage y hitbox

--- Lurkers of the Dark/scenes/player.gd
+++ Lurkers of the Dark/scenes/player.gd
@@ -6,15 +6,26 @@ extends CharacterBody2D
 @export var bullet_scene: PackedScene
 
 @export var speed = 200
-@export var health = 100
-@export var curr_health = health
+@export var health = 100:
+	get:
+		return health
+	set(val):
+		health = max(val, 0)
+		if health <= 0:
+			dead = true
 
-var dead = false
+var dead = false:
+	get:
+		return dead
+	set(val):
+		dead = val
+
 
 @export var score = 1 :
 	set(value):
 		score = value
 		Debug.log("Player %s score %d" % [name, score])
+
 
 func _physics_process(delta: float) -> void:
 	if is_multiplayer_authority():
@@ -22,6 +33,7 @@ func _physics_process(delta: float) -> void:
 		velocity = move_dir * speed
 		send_data.rpc(global_position, velocity)
 	move_and_slide()
+
 
 func _input(event: InputEvent) -> void:
 	if is_multiplayer_authority():
@@ -33,16 +45,13 @@ func _input(event: InputEvent) -> void:
 			# triggers syncronizer
 			score += 1
 
+
 func setup(player_data: Statics.PlayerData):
 	name = str(player_data.id)
 	set_multiplayer_authority(player_data.id)
 	multiplayer_spawner.set_multiplayer_authority(player_data.id)
 	multiplayer_synchronizer.set_multiplayer_authority(player_data.id)
 	
-func receive_dmg(dmg: int):
-	curr_health =- dmg
-	if curr_health == 0:
-		kill()
 
 func kill():
 	if dead:
@@ -52,9 +61,16 @@ func kill():
 	$Graphics/Dead.show()
 	z_index = -1
 
+
+func take_damage(damage):
+	if is_multiplayer_authority():
+		health -= damage
+
+
 func shooter_player():
 	pass	
-	
+
+
 @rpc("authority", "call_local", "reliable")
 func test(name):
 	var message = "test " + name
@@ -62,6 +78,7 @@ func test(name):
 	var sender_player = Game.get_player(sender_id)
 	Debug.log(message)
 	Debug.log(sender_player.name)
+
 
 @rpc
 func send_data(pos: Vector2, vel: Vector2):
--- Lurkers of the Dark/scenes/zombie.gd
+++ Lurkers of the Dark/scenes/zombie.gd
@@ -1,47 +1,101 @@
 extends CharacterBody2D
 
-var move_speed = 50
-@export var health = 100
-var damage = 10
+@export var dead = false:
+	get:
+		return dead
+	set(val):
+		dead = val
+		
+@export var health = 100:
+	get:
+		return health
+	set(val):
+		health = max(val, 0)
+		if health <= 0:
+			dead = true
+		update_health()
 
-var dead = false
+var move_speed = 150
+var damage = 10
+@onready var attack_cooldown = $Attack_Cooldown
+
 var player_in_area = false
-var player = null
+var players = []
+var player_in_attack_range  = false
+var players_being_attacked = []
 
 
 func _ready():
 	dead = false
 
 
-func _physics_process(delta):	
+func _physics_process(delta):
+	zombie_movement()
+	attack_player()
+	
+	
+func zombie_movement():
 	if !dead:
 		$Detection_Area/CollisionShape2D.disabled = false
 		if player_in_area:
-			position += (player.position - position) / move_speed
+			position += (players[0].position - position) / move_speed
 		else:
 			pass
 	
 	if dead:
 		$Detection_Area/CollisionShape2D.disabled = true
-
+	
 
 func _on_detection_area_body_entered(body):
 	if body.has_method("shooter_player"):
-		print("ENTERED")
 		player_in_area = true
-		player = body
+		players.push_back(body)
 
 
 func _on_detection_area_body_exited(body):
 	if body.has_method("shooter_player"):
-		player_in_area = false
-		player = null
+		players.erase(body)
+		if players.is_empty():
+			player_in_area = false
 
+
+func _on_hitbox_body_entered(body):
+	if body.has_method("shooter_player"):
+		player_in_attack_range = true
+		players_being_attacked.push_back(body)
+
+
+func _on_hitbox_body_exited(body):
+	if body.has_method("shooter_player"):
+		players_being_attacked.erase(body)
+		if players_being_attacked.is_empty():
+			player_in_attack_range = false
+
+
+func attack_player():
+	if player_in_attack_range and attack_cooldown.is_stopped():
+		players_being_attacked[0].take_damage(20)
+
+
+func take_damage(damage):
+	if is_multiplayer_authority():
+		health -= damage
+					
+
+func update_health():
+	var health_bar = $HealthBar
+	health_bar.value = health
+	
+	if health >= 100:
+		health_bar.visible = false
+	else:
+		health_bar.visible = true
+
+
+func enemy():
+	pass
 
 @rpc
 func send_data(pos: Vector2, vel: Vector2):
 	global_position = lerp(global_position, pos, 0.75)
 	velocity = lerp(velocity, vel, 0.75)
-
-
-		
