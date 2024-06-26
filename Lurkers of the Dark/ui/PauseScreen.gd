c5eb4b9df36c7214cf5eb57ea89c143bfb46b813
BaconXBX <hugoca.2000@gmail.com>
2024-06-25 21:51:47 -0400

    pause menu

--- Lurkers of the Dark/ui/PauseScreen.gd
+++ Lurkers of the Dark/ui/PauseScreen.gd
@@ -3,13 +3,17 @@ extends CanvasLayer
 @onready var pause_screen: Control = $PauseScreen
 
 
-func esc():
-	Debug.log("esc pressed")
+func _process(delta):
+	testEsc()
+
+
+func testEsc():
 	if Input.is_action_just_pressed("esc") and pause_screen.visible:
 		pause_screen.hide()
-		
+		Debug.log("esc pressed")
 	if Input.is_action_just_pressed("esc") and !pause_screen.visible:
 		pause_screen.show()
+		Debug.log("esc pressed")
 
 
 func _on_resume_pressed():
